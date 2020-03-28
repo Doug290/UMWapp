a5f6c7d1df64ea935b29ca2225525c18f98dd3b6
Douglas Amorim <Douglas.Amorim@fit-tecnologia.org.br>
2020-03-28 18:09:16 -0300

    starting spring boot api project

---
+++ umwa_api/src/test/java/com/douglas/umwa/UmwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.umwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmwaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
