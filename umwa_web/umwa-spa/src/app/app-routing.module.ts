207d0f50ce2d0009dbedd8a50a979806c97b9a11
Douglas Amorim <Douglas.Amorim@fit-tecnologia.org.br>
2020-03-28 19:59:42 -0300

    configuring CORS and testing api comm

--- umwa_web/umwa-spa/src/app/app-routing.module.ts
+++ umwa_web/umwa-spa/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyComponentComponent } from './components/my-component/my-component.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: MyComponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
